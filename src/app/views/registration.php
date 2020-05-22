b7d4931e1d8ec2e59a090aea410c1733268518ea
DanilchikB <danil1357997@gmail.com>
2020-05-22 21:24:45 +0300

    layout html errors fixed

--- src/app/views/registration.php
+++ src/app/views/registration.php
@@ -2,17 +2,17 @@
 
     <h1>Регистрация</h1>
     <form id = "registration-form" v-on:submit.prevent="sendingRegistration" action = "/check/registration" method = "post">
-        <input type = "text" name="login" v-model="login" placeholder="Логин"></br>
+        <input type = "text" name="login" v-model="login" placeholder="Логин"><br/>
         <div class = "message" v-html="loginMessage"></div>
 
-        <input type = "password" name = "password" v-model="password" placeholder="Пароль"></br>
+        <input type = "password" name = "password" v-model="password" placeholder="Пароль"><br/>
         <div class = "message" v-html="passwordMessage"></div>
         
-        <input type = "password" name = "repeat-password" v-model="passwordRepeat" placeholder="Повторите пароль"></br>
+        <input type = "password" name = "repeat-password" v-model="passwordRepeat" placeholder="Повторите пароль"><br/>
         <div class = "message" v-html="passwordRepeatMessage"></div>
 
         <input type="submit" value="Зарегистрироваться">
 
-    </from>
+    </form>
 
 </div>
